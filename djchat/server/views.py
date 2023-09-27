de015e98d4dc0370bfea8924aadfda938764055c
JimmyFu <996367062@qq.com>
2023-09-27 08:52:34 +0800

    create api by serves id

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import Server
 from .serializer import ServerSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 
 class ServerListViewSet(viewsets.ViewSet):
 
@@ -12,8 +13,10 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get('cagetory')
         qty = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == 'true'
+        by_serverid = request.query_params.get('by_serverid')
+        if by_user and not request.user.is_authenticated:
+            raise AuthenticationFailed()
 
-    
         if category:
             self.queryset = self.queryset.filter(category__name = category)
 
@@ -24,7 +27,13 @@ class ServerListViewSet(viewsets.ViewSet):
         if qty:
             self.queryset = self.queryset[:int(qty)]
 
-        
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id{by_serverid} not found")
+            except ValueError:
+                raise ValidationError(detail="Server value error")
     
         serializer = ServerSerializer(self.queryset, many = True)
         return Response(serializer.data)
